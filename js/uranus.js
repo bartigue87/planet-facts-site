ef482d76ae4fbe76c524b87b19d6e30e0f352478
Brandon Artigue <bartigue87@yahoo.com>
2022-09-16 19:37:45 -0600

    finish uranus

--- js/uranus.js
+++ js/uranus.js
@@ -5,11 +5,11 @@ const overviewBtn = document.getElementById("overview-btn");
 const internalBtn = document.getElementById("internal-btn");
 const surfaceBtn = document.getElementById("surface-btn");
 const overviewText =
-  "Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty. As the brightest natural object in Earth's night sky after the Moon, Venus can cast shadows and can be, on rare occasions, visible to the naked eye in broad daylight.";
+  "Uranus is the seventh planet from the Sun. Its name is a reference to the Greek god of the sky, Uranus according to Greek mythology, was the great-grandfather of Ares. It has the third-largest planetary radius and fourth-largest planetary mass in the Solar System.";
 const internalText =
-  "The similarity in size and density between Venus and Earth suggests they share a similar internal structure: a core, mantle, and crust. Like that of Earth, Venusian core is most likely at least partially liquid because the two planets have been cooling at about the same rate.";
+  "The standard model of Uranus's structure is that it consists of three layers: a rocky (silicate/iron–nickel) core in the centre, an icy mantle in the middle and an outer gaseous hydrogen/helium envelope. The core is relatively small, with a mass of only 0.55 Earth masses.";
 const surfaceText =
-  "Much of the Venusian surface appears to have been shaped by volcanic activity. Venus has several times as many volcanoes as Earth, and it has 167 large volcanoes that are over 100 km (60 mi) across. The only volcanic complex of this size on Earth is the Big Island of Hawaii.";
+  "The composition of Uranus's atmosphere is different from its bulk, consisting mainly of molecular hydrogen and helium. The helium molar fraction, i.e. the number of helium atoms per molecule of gas, is 0.15±0.03 in the upper troposphere.";
 const navItems = document.getElementById("nav-items");
 const infoBoxes = document.querySelector(".info-boxes-container");
 
