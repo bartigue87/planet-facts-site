91de7bc93ef5aaa90dad602967cff9ec2073945f
Brandon Artigue <bartigue87@yahoo.com>
2022-09-16 18:47:49 -0600

    finish jupiter

---
+++ js/jupiter.js
@@ -0,0 +1,45 @@
+const planetImage2 = document.getElementById("planet-image2");
+const planetImage3 = document.getElementById("planet-image3");
+const mainText = document.getElementById("main-text");
+const overviewBtn = document.getElementById("overview-btn");
+const internalBtn = document.getElementById("internal-btn");
+const surfaceBtn = document.getElementById("surface-btn");
+const overviewText =
+  "Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass two and a half times that of all the other planets in the Solar System combined, but less than one-thousandth the mass of the Sun.";
+const internalText =
+  "When the Juno arrived in 2016, it found that Jupiter has a very diffuse core that mixes into its mantle. A possible cause is an impact from a planet of about ten Earth masses a few million years after Jupiter's formation, which would have disrupted an originally solid Jovian core.";
+const surfaceText =
+  "The best known feature of Jupiter is the Great Red Spot, a persistent anticyclonic storm located 22° south of the equator. It is known to have existed since at least 1831, and possibly since 1665.";
+const navItems = document.getElementById("nav-items");
+const infoBoxes = document.querySelector(".info-boxes-container");
+
+function handleOverviewBtnClick() {
+  internalBtn.classList.remove("active");
+  surfaceBtn.classList.remove("active");
+  overviewBtn.classList.add("active");
+  planetImage2.style.visibility = "hidden";
+  planetImage3.style.visibility = "hidden";
+  mainText.textContent = overviewText;
+}
+
+function handleInternalBtnClick() {
+  overviewBtn.classList.remove("active");
+  surfaceBtn.classList.remove("active");
+  internalBtn.classList.add("active");
+  planetImage2.style.visibility = "visible";
+  planetImage3.style.visibility = "hidden";
+  mainText.textContent = internalText;
+}
+
+function handleSurfaceBtnClick() {
+  internalBtn.classList.remove("active");
+  overviewBtn.classList.remove("active");
+  surfaceBtn.classList.add("active");
+  planetImage2.style.visibility = "hidden";
+  planetImage3.style.visibility = "visible";
+  mainText.textContent = surfaceText;
+}
+
+overviewBtn.addEventListener("click", handleOverviewBtnClick);
+internalBtn.addEventListener("click", handleInternalBtnClick);
+surfaceBtn.addEventListener("click", handleSurfaceBtnClick);
